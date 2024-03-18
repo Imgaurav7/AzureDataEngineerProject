2ff9672f49568d3a29ae810bd3602f5ed30ce62b
Imgaurav7 <Imgaurav7>
2024-03-18 08:35:18 +0000

    mount

--- mount_storage.py
+++ mount_storage.py
@@ -1,6 +1,6 @@
 # Databricks notebook source
 configs = {
-  "fs.azure.account.auth.type": "CustomAccessToken",
+  "fs.azure.account.auth.typ0e": "CustomAccessToken",
   "fs.azure.account.custom.token.provider.class": spark.conf.get("spark.databricks.passthrough.adls.gen2.tokenProviderClassName")
 }
 
